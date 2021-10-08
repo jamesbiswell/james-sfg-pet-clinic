db2c112d1bc64aa29af85b9f13a3f7678fb78fe8
jamesbiswell <jamesbiswell@aol.com>
2021-10-08 11:14:40 +0100

    Section 4.51 Spring Pet Clinic - Implement Base Entity Closes #10

--- pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.jamesbiswell.jamessfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.jamesbiswell.jamessfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
