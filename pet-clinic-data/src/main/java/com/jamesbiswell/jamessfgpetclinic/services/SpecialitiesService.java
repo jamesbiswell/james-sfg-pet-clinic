71c9c19569a3e50666b3a941ce741f9808edf9c8
jamesbiswell <jamesbiswell@aol.com>
2021-10-12 16:40:43 +0100

    Section 7.125 Spring Pet Clinic - Create Speciality Map Service Impl Closes #31

---
+++ pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.jamesbiswell.jamessfgpetclinic.services;
+
+import com.jamesbiswell.jamessfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService <Speciality, Long> {
+}
