fb07f67329dc85580adbe8c8b9c821717a4f234e
jamesbiswell <jamesbiswell@aol.com>
2021-10-12 17:05:03 +0100

    Section 7.126 Spring Pet Clinic - Create Specialties, Add to Vets (Startup) Closes #34

--- pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/services/SpecialityService.java
+++ pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/services/SpecialityService.java
@@ -2,5 +2,5 @@ package com.jamesbiswell.jamessfgpetclinic.services;
 
 import com.jamesbiswell.jamessfgpetclinic.model.Speciality;
 
-public interface SpecialitiesService extends CrudService <Speciality, Long> {
+public interface SpecialityService extends CrudService <Speciality, Long> {
 }
--- pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/services/map/SpecialityMapService.java
+++ pet-clinic-data/src/main/java/com/jamesbiswell/jamessfgpetclinic/services/map/SpecialityMapService.java
@@ -1,13 +1,13 @@
 package com.jamesbiswell.jamessfgpetclinic.services.map;
 
 import com.jamesbiswell.jamessfgpetclinic.model.Speciality;
-import com.jamesbiswell.jamessfgpetclinic.services.SpecialitiesService;
+import com.jamesbiswell.jamessfgpetclinic.services.SpecialityService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
     @Override
     public Set<Speciality> findAll() {
         return super.findAll();
